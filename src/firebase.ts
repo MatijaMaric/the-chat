240d98b1f84179250a136dee788db6064cd1f44b
Matija Marić <sirSlani@gmail.com>
2019-01-27 18:57:40 +0100

    Circle CI.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
-export function initFirebase() {
-  const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-  };
+const config = {
+  apiKey: "AIzaSyC0cFsCUU2S_MyGIA16p_J2PBwDa5Gxi5w",
+  authDomain: "the-chat-d9454.firebaseapp.com",
+  databaseURL: "https://the-chat-d9454.firebaseio.com",
+  projectId: "the-chat-d9454",
+  storageBucket: "the-chat-d9454.appspot.com",
+  messagingSenderId: "513329153661"
+};
 
-  firebase.initializeApp(config);
-}
+firebase.initializeApp(config);
+
+export default firebase;
