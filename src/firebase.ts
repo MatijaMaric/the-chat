d7d4ad8d03597ebbc45b7024dee0b8e27fea4950
Matija Marić <sirSlani@gmail.com>
2019-01-27 21:28:31 +0100

    firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,5 @@
 import * as firebase from "firebase/app";
-
-const config = {
-  apiKey: "AIzaSyC0cFsCUU2S_MyGIA16p_J2PBwDa5Gxi5w",
-  authDomain: "the-chat-d9454.firebaseapp.com",
-  databaseURL: "https://the-chat-d9454.firebaseio.com",
-  projectId: "the-chat-d9454",
-  storageBucket: "the-chat-d9454.appspot.com",
-  messagingSenderId: "513329153661"
-};
+import { result as config } from "./firebase-config.json";
 
 firebase.initializeApp(config);
 
