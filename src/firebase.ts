14578ab7721ea57acf7c4b9f1ac25297aa4fb314
Matija Marić <sirSlani@gmail.com>
2019-01-27 21:29:44 +0100

    Revert "firebase config"
    This reverts commit d7d4ad8d03597ebbc45b7024dee0b8e27fea4950.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,13 @@
 import * as firebase from "firebase/app";
-import { result as config } from "./firebase-config.json";
+
+const config = {
+  apiKey: "AIzaSyC0cFsCUU2S_MyGIA16p_J2PBwDa5Gxi5w",
+  authDomain: "the-chat-d9454.firebaseapp.com",
+  databaseURL: "https://the-chat-d9454.firebaseio.com",
+  projectId: "the-chat-d9454",
+  storageBucket: "the-chat-d9454.appspot.com",
+  messagingSenderId: "513329153661"
+};
 
 firebase.initializeApp(config);
 
