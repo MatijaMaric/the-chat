0d74180ba1e15c2c602ef2338980e63b89dac290
Matija Marić <sirSlani@gmail.com>
2019-01-27 19:34:33 +0100

    bug fix.

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { initFirebase } from "./firebase";
+import firebase from "./firebase";
 
-initFirebase();
+console.log(firebase.SDK_VERSION);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
