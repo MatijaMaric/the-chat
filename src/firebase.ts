198833a40f05e14f051a06c97bbdb2e485265a17
Matija Marić <sirSlani@gmail.com>
2019-01-27 11:10:04 +0100

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 export function initFirebase() {
   const config = {
-    apiKey: "AIzaSyC0cFsCUU2S_MyGIA16p_J2PBwDa5Gxi5w",
-    authDomain: "the-chat-d9454.firebaseapp.com",
-    databaseURL: "https://the-chat-d9454.firebaseio.com",
-    projectId: "the-chat-d9454",
-    storageBucket: "the-chat-d9454.appspot.com",
-    messagingSenderId: "513329153661"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
   firebase.initializeApp(config);
