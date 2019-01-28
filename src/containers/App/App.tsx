6dae43f48fa3868eb629bab1855c81aa88656011
Matija Marić <sirSlani@gmail.com>
2019-01-28 23:57:39 +0100

    Appbar

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -8,6 +8,9 @@ import LoginScreen from "../LoginScreen/LoginScreen";
 import Button from "@material-ui/core/Button";
 import firebase from "../../firebase";
 import { User } from "../../common/models";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { Avatar, Typography } from "@material-ui/core";
 
 export interface AppStateProps {
   isAuthenticated: boolean;
@@ -39,6 +42,7 @@ function mapDispatchToProps(dispatch: Dispatch<RootAction>): AppDispatchProps {
 
 class App extends React.Component<AppProps & AppStateProps & AppDispatchProps> {
   public render() {
+    return this._renderAppBar();
     if (this.props.isAuthenticated) {
       return (
         <div>
@@ -52,6 +56,30 @@ class App extends React.Component<AppProps & AppStateProps & AppDispatchProps> {
           <LoginScreen />
         </div>
       );
+    }
+  }
+
+  private _renderAppBar(): React.ReactNode {
+    return (
+      <AppBar position="static">
+        <Toolbar>{this._renderUserInfo()}</Toolbar>
+      </AppBar>
+    );
+  }
+
+  private _renderUserInfo(): React.ReactNode {
+    if (this.props.isAuthenticated) {
+      return (
+        <>
+          <Typography color="inherit">{this.props.displayName}</Typography>
+          <Avatar src={this.props.avatarURL} />
+          <Button color="inherit" onClick={this._handleSignOut}>
+            Sign out
+          </Button>
+        </>
+      );
+    } else {
+      return <LoginScreen />;
     }
   }
 
